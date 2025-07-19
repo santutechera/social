a869ab84b90cb029f002835bc2963d7b21312761
Santiago Techera Amestoy <santiago@kanban.uy>
2025-07-18 23:51:48 -0300

    Initial commit

---
+++ mail_send_confirmation/readme/CONTRIBUTORS.md
@@ -0,0 +1,6 @@
+- [Quartile](https://www.quartile.co):
+  - Aung Ko Ko Lin
+- [360ERP](https://www.360erp.com):
+  - Andrea Stirpe
+- [CorporateHub](https://corporatehub.eu/)
+  - Alexey Pelykh \<<alexey.pelykh@corphub.eu>\>
---
+++ mass_mailing_list_dynamic/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import mail_thread_blacklist
+from . import mailing
+from . import mailing_contact
+from . import mailing_list
+from . import res_partner
